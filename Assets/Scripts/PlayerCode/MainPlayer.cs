29fb0d101fd227cdc9eb8196c1805addcc5d2278
Shadman Syed <shadman3d@gmail.com>
2024-12-17 23:23:13 -0500

    Two Cameras and a confusing input system

--- Assets/Scripts/PlayerCode/MainPlayer.cs
+++ Assets/Scripts/PlayerCode/MainPlayer.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MainPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Camera Settings")]
+    public InputAction switchCamAction;
+    public Camera firstCam;
+    public Camera secondCam;
+    public bool isFirstCamActive = true;
+
     void Start()
+    {
+        firstCam.enabled = true;
+        secondCam.enabled = false;
+        switchCamAction.performed += (ctx) => {
+            isFirstCamActive = !isFirstCamActive;
+            SwitchCam(isFirstCamActive);
+        };
+    }
+
+    void SwitchCam(bool isFirstCamActive) {
+        firstCam.enabled = isFirstCamActive;
+        secondCam.enabled = !isFirstCamActive;
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        switchCamAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        switchCamAction.Disable();
     }
 }
