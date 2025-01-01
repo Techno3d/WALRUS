4334503e86d8caa62274147f9bfe9d61567fdd80
Shadman Syed <shadman3d@gmail.com>
2025-01-01 00:22:53 -0500

    Basterdized markov process tbf, but should work

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,27 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
+// using static Unity.Mathematics.math;
 
 public class Enemy : MonoBehaviour
 {
     EnemyState state = EnemyState.Moving;
-    float[,] markovModel = {
+    // This is kind of a markov model
+    // Essentially, using the player stats array, I will change this matrix. Not really a POMDP or an HMM
+    float[,] referenceModel = {
     //   C    M    F    L
-        {0,   0,   0,   0}, // EnemyState.Corrupting
-        {0,   0,   0,   0}, // EnemyState.Moving
-        {0,   0,   0,   0}, // EnemyState.Fleeing
-        {0,   0,   0,   0}  // EnemyState.Listening
+        {0,   1.0f,0.0f,0}, // EnemyState.Corrupting
+        {0,   0.9f,0,   0.1f}, // EnemyState.Moving
+        {0.05f,0.5f,0.3f,  0.3f}, // EnemyState.Fleeing
+        {0.01f,0.99f,0, 0}  // EnemyState.Listening
     };
+    float[,] actualModel = {
+    //   C    M    F    L
+        {0,   0.5f,0.5f,0}, // EnemyState.Corrupting
+        {0,   0.9f,0,   0.1f}, // EnemyState.Moving
+        {0.05f,0.5f,0.3f,  0.3f}, // EnemyState.Fleeing
+        {0.01f,0.99f,0, 0}  // EnemyState.Listening
+    };
+    float timeClock = 0f;
+
+    [Tooltip("This controls how long the AI has to do an action before switching")]
+    public float actionTime = 1f;
     // This code controls if the AI thinks a player is active/inactive
     [Header("Player Detection")]
     public GameObject body1, body2;
     public float SightDistance = 15f;
     public float HearingDistance = 10f;
     public float MisHearingChance = 0.1f;
-    public float MaxListenTime = 2f;
-    [Tooltip("The penalty used by the AI when it does bad")]
-    public float penalty = -5;
-    float listeningTime = 0f;
     PlayerStats[] statsArr = new PlayerStats[2];
 
     void Start()
@@ -41,7 +51,6 @@ public class Enemy : MonoBehaviour
 
             case EnemyState.Listening:
                 // Listening code
-                listeningTime += Time.deltaTime;
                 break;
 
             case EnemyState.Moving:
@@ -53,15 +62,84 @@ public class Enemy : MonoBehaviour
                 break;
         }
 
-        UpdatePlayerStats(body1, statsArr[1]);
-        UpdatePlayerStats(body2, statsArr[2]);
-        SwitchStates();
+        timeClock += Time.deltaTime;
+        if(timeClock > actionTime) {
+            UpdatePlayerStats(body1, statsArr[1]);
+            UpdatePlayerStats(body2, statsArr[2]);
+            updateActualModel();
+            SwitchStates();
+            timeClock = 0f;
+        }
     }
     
+    void updateActualModel() {
+        float activePercentage = -1f;
+        if(statsArr[0].visibility != PlayerVisibility.NotVisible && statsArr[1].visibility != PlayerVisibility.NotVisible) {
+            activePercentage = Mathf.Max(statsArr[0].PercentInactive, statsArr[1].PercentInactive);
+        } else if(statsArr[0].visibility != PlayerVisibility.NotVisible) {
+            activePercentage = statsArr[0].PercentInactive;
+        } else if(statsArr[1].visibility != PlayerVisibility.NotVisible) {
+            activePercentage = statsArr[1].PercentInactive;
+        } else {
+            actualModel = referenceModel;
+            return;
+        }
+
+        float activePercentageCorrupting = mapRange(
+            activePercentage, 
+            0, 1, 
+            0, 1
+                -actualModel[(int)EnemyState.Corrupting, (int)EnemyState.Listening]
+                -actualModel[(int)EnemyState.Corrupting, (int)EnemyState.Corrupting]
+        );
+        actualModel[(int)EnemyState.Corrupting, (int)EnemyState.Moving] = 1-activePercentageCorrupting;
+        actualModel[(int)EnemyState.Corrupting, (int)EnemyState.Fleeing] = activePercentageCorrupting;
+
+        float activePercentageMoving = mapRange(
+            activePercentage, 
+            0, 1, 
+            0, 1
+                -actualModel[(int)EnemyState.Moving, (int)EnemyState.Listening]
+                -actualModel[(int)EnemyState.Moving, (int)EnemyState.Corrupting]
+        );
+        actualModel[(int)EnemyState.Moving, (int)EnemyState.Moving] = 1-activePercentageMoving;
+        actualModel[(int)EnemyState.Moving, (int)EnemyState.Fleeing] = activePercentageMoving;
+
+        float activePercentageFleeing = mapRange(
+            activePercentage, 
+            0, 1, 
+            0, 1
+                -actualModel[(int)EnemyState.Fleeing, (int)EnemyState.Listening]
+                -actualModel[(int)EnemyState.Fleeing, (int)EnemyState.Corrupting]
+        );
+        actualModel[(int)EnemyState.Fleeing, (int)EnemyState.Moving] = 1-activePercentageFleeing;
+        actualModel[(int)EnemyState.Fleeing, (int)EnemyState.Fleeing] = activePercentageFleeing;
+
+        float activePercentageListening = mapRange(
+            activePercentage, 
+            0, 1, 
+            0, 1
+                -actualModel[(int)EnemyState.Listening, (int)EnemyState.Listening]
+                -actualModel[(int)EnemyState.Listening, (int)EnemyState.Corrupting]
+        );
+        actualModel[(int)EnemyState.Listening, (int)EnemyState.Moving] = 1-activePercentageListening;
+        actualModel[(int)EnemyState.Listening, (int)EnemyState.Fleeing] = activePercentageListening;
+    }
+
+    float mapRange(float x, float in_min, float in_max, float out_min, float out_max)
+    {
+      return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+    
+    // I think this is it
     void SwitchStates() {
-        // Can't listen if they've been listening too long
-        if(listeningTime > MaxListenTime) {
-            listeningTime = 0f;
+        float maxPercent = -1;
+        for(int i = 0; i < 4; i++) {
+            float percentCompare = actualModel[(int)state,i];
+            if(percentCompare>maxPercent) {
+                maxPercent = percentCompare;
+                state = (EnemyState)i;
+            }
         }
     }
 
@@ -90,11 +168,20 @@ public class Enemy : MonoBehaviour
                 stats.PercentInactive = 1.00f;
         } else if(state == EnemyState.Listening && stats.visibility != PlayerVisibility.NotVisible) {
             bool failedHearing = stats.visibility == PlayerVisibility.CanHear && Random.Range(0f, 1f) < MisHearingChance;
-            stats.MovementDelta = failedHearing
-                ? 0
-                : Vector3.Distance(stats.oldPos, body.transform.position);
-            stats.oldPos = failedHearing ? stats.oldPos : body.transform.position;
-            
+            if(!failedHearing) {
+                stats.MovementDelta = Vector3.Distance(stats.oldPos, body.transform.position);
+                stats.oldPos = body.transform.position;
+                if(stats.MovementDelta > 0) {
+                    Debug.Log(stats.MovementDelta);
+                    stats.PercentInactive += 0.2f;
+                    stats.PercentInactive = Mathf.Clamp(stats.PercentInactive, 0f, 1f);
+                } else {
+                    stats.PercentInactive -= 0.2f;
+                    stats.PercentInactive = Mathf.Clamp(stats.PercentInactive, 0f, 1f);
+                }
+            } else {
+                stats.MovementDelta = 0;
+            }
         }
 
     }
@@ -112,5 +199,17 @@ enum PlayerVisibility {
 }
 
 enum EnemyState {
-    Corrupting, Moving, Fleeing, Listening
+    Corrupting = 0, Moving = 1, Fleeing = 2, Listening = 3
 }
+
+/*
+enum POMDPState {
+    Moving = -1, 
+    Goal = 100, 
+    Listening = -5, 
+    Corrupting = -2,
+    //Both of these refer to when the ai tries to run past a player cell
+    Caught = -25, 
+    Correct = 25,
+}
+*/
