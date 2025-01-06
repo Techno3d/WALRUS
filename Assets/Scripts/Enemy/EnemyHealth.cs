4d47713a01e998e5bd17a0b9e60bc762da14767b
ElishaCahan <egordoncahan@gmail.com>
2025-01-05 20:18:48 -0500

    destroy

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -32,7 +32,7 @@ public class EnemyHealth : MonoBehaviour
         if (health <= 0)
         {
             EnemyDeath?.Invoke();
-            // Destroy(gameObject);
+            Destroy(gameObject);
         }
     }
 }
