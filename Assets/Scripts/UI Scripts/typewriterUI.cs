042d54142dfc1b59b7fbefa725cb695dcfac94b3
ElishaCahan <egordoncahan@gmail.com>
2025-01-06 22:53:14 -0500

    audio for lore done wahoo!

--- Assets/Scripts/UI Scripts/typewriterUI.cs
+++ Assets/Scripts/UI Scripts/typewriterUI.cs
@@ -18,10 +18,23 @@ public class typewriterUI : MonoBehaviour
     [SerializeField] float timeBtwChars = 0.1f;
     [SerializeField] string leadingChar = "";
     [SerializeField] bool leadingCharBeforeDelay = false;
+    [SerializeField] private AudioClip audioClip;
+    [SerializeField] private AudioClip audioClip2;
+
+    // Reference to the AudioSource component
+    private AudioSource audioSource;
+    private AudioSource audioSource2;
+
 
     // Use this for initialization
     void Start()
     {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource2 = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = audioClip;
+        audioSource2.clip = audioClip2;
+        audioSource.loop = true;
+        audioSource2.loop = true;
         _text = GetComponent<Text>()!;
         _tmpProText = GetComponent<TMP_Text>()!;
 
@@ -44,6 +57,7 @@ public class typewriterUI : MonoBehaviour
 
     IEnumerator TypeWriterText()
     {
+        audioSource.Play();
         _text.text = leadingCharBeforeDelay ? leadingChar : "";
 
         yield return new WaitForSeconds(delayBeforeStart);
@@ -63,28 +77,54 @@ public class typewriterUI : MonoBehaviour
         {
             _text.text = _text.text.Substring(0, _text.text.Length - leadingChar.Length);
         }
+        audioSource.Stop();
     }
 
     IEnumerator TypeWriterTMP()
     {
+        audioSource.Play();
         _tmpProText.text = leadingCharBeforeDelay ? leadingChar : "";
-
+        int count = 0;
         yield return new WaitForSeconds(delayBeforeStart);
-
+        bool hasdone = false;
         foreach (char c in writer)
         {
             if (_tmpProText.text.Length > 0)
             {
+
                 _tmpProText.text = _tmpProText.text.Substring(0, _tmpProText.text.Length - leadingChar.Length);
             }
             _tmpProText.text += c;
             _tmpProText.text += leadingChar;
+            if (count > 4)
+            {
+                Debug.Log(count);
+                if (_tmpProText.text.Contains("...") && hasdone == false)
+                {
+                    audioSource.Stop();
+                    audioSource2.Play();
+                    hasdone = true;
+                }
+            }
+            count++;
             yield return new WaitForSeconds(timeBtwChars);
         }
 
         if (leadingChar != "")
         {
             _tmpProText.text = _tmpProText.text.Substring(0, _tmpProText.text.Length - leadingChar.Length);
+        }
+        audioSource.Stop();
+    }
+    private void OnDestroy()
+    {
+        if (audioSource && audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+        if (audioSource2 && audioSource2.isPlaying)
+        {
+            audioSource2.Stop();
         }
     }
 }
